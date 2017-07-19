21ac5dab980df3e82b7d84a1467532955efcc649
Jorrit Elfferich <mrjorrit@gmail.com>
2017-07-19 22:53:29 +0200

    initial commit, pushing to github.

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var
+  consoleLog = false,
+  nodeEnv = (process.env.NODE_ENV || '').trim().toLowerCase(),
+  devBuild = nodeEnv !== 'production',
+  pkg = require('./package.json'),
+  dir = {
+    base: __dirname + '/',
+    lib: __dirname + '/lib/',
+    source: './src/',
+    dest: './build/'
+  },
+  metalsmith = require('metalsmith'),
+  layouts = require('metalsmith-layouts'),
+  assets = require('metalsmith-assets'),
+  webpack = require('metalsmith-webpack'),
+  ignore = require('metalsmith-ignore'),
+  path = require('path'),
+  browsersync = devBuild ? require('metalsmith-browser-sync') : null,
+  siteMeta = {
+    devBuild: devBuild,
+    version: pkg.version,
+    nodeEnv: nodeEnv,
+    title: 'Jorrit Elfferich',
+    desc: 'Personal site of Jorrit Elfferich',
+    author: 'Jorrit Elfferich',
+    contact: 'jorrit@elfferi.ch'
+  },
+  templateConfig = {
+    engine: 'pug',
+    pretty: true
+  };
+
+var ms = metalsmith(dir.base)
+  .metadata(siteMeta)
+  .clean(!devBuild)
+  .use(webpack({
+    context: dir.source + 'js/',
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, './build/js/'),
+      filename: 'bundle.js'
+    }
+  }))
+  .use(ignore([
+    'js/*'
+  ]))
+  .source(dir.source)
+  .destination(dir.dest)
+  .use(require('metalsmith-sense-sass')({
+    sass: {
+      outFile: path.resolve(__dirname, './build/js/style.css')
+    },
+    postcss: {
+      plugins: {
+        'autoprefixer': {}
+      }
+    }
+  }))
+  .use(layouts(templateConfig));
+
+
+
+if (browsersync) ms.use(browsersync({ 
+  server: dir.dest,
+  files: [dir.source + '**/*', dir.base + 'assets/', dir.base + 'layouts'],
+  serveStatic: ['./build'],
+  serveStaticOptions: {
+      extensions: ['html'] 
+  }
+}));
+
+ms
+  .use(assets({
+    source: dir.base + 'assets/',
+    destination: './'
+  }))
+  .build(function(err) {
+    if (err) throw err;
+  });
---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+window.$ = window.jQuery = require('jquery');
+require("jquery-sticky");
+let Typed = require("typed.js");
+let previousColor = localStorage.getItem("previousColor");
+let colors = ['red', 'green', 'blue', 'purple'];
+let indexPreviousColor = colors.indexOf(previousColor);
+if (indexPreviousColor > -1) {colors.splice(indexPreviousColor, 1);}
+let randomColor = colors[Math.floor(Math.random() * colors.length)];
+localStorage.setItem("previousColor", randomColor);
+
+let manConsoleScrolledIntoView = false;
+
+$(document).ready(function(){
+  $(".randomColor").addClass(randomColor);
+  $(".randomColorBackground").addClass(randomColor);
+  $(".randomColorMenu").addClass(randomColor);
+  $("#navigation").sticky({topSpacing:0,bottomSpacing:0,zIndex:2});
+  $('#navigation').on('sticky-start', function() {
+    $("#nav-logo").removeClass('hidden');
+  });
+  $('#navigation').on('sticky-end', function() {
+    $("#nav-logo").addClass('hidden');
+  });
+  $('#expand-man-console').click(function(){
+    $('#man-console .body').css('height', 'auto');
+    $(this).parent().addClass('hidden');
+    $(this).parent().next().removeClass('hidden');
+  });
+  $('.revealExcerpt').click(function(){
+    $(this).next().removeClass('hidden');
+  });
+  $('.hideExcerpt').click(function(){
+    $(this).parent().addClass('hidden');
+  });
+  $('#menu-button button').click(function(){
+    var display = 'block';
+    if($(this).hasClass("is-active")){
+      display = 'none';
+    }
+    $(this).toggleClass('is-active');
+    $('.mobile-menu').css('display', display);
+  });
+
+});
+
+var typed = new Typed("#typed", {
+  stringsElement: '#typed-strings',
+  typeSpeed: 100,
+  loop: false,
+  cursorChar: '',
+  onComplete: function(self) {
+    setTimeout(function(){
+      $('#man-console .body .custom-typed-cursor').addClass('hidden');
+      $('.man-result').removeClass('hidden');
+    }, 250);
+  }
+});
+typed.stop();
+
+
+$(window).scroll(function(){
+  function elementScrolled(elem)
+  {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top + 60;
+    return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  if(!manConsoleScrolledIntoView && elementScrolled('#man-console')) {
+    manConsoleScrolledIntoView = true;
+    typed.start();
+  }
+});
+
+
+$('a[href^="#"]').on('click', function(event) {
+  console.log(event);
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+    event.preventDefault();
+    var navHeight = ($('#navigation').height() + $('.mobile-menu').height());
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top - (navHeight + 15)
+    }, 1000);
+  }
+});
